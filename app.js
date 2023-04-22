460730be5791b7290cbf89e20d6179f3fcc8177a
Kristina Morozova <christina.vadi@gmail.com>
2023-04-22 16:29:26 +0600

    refactor(extended-1.2): add alt attribute for img in html

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="delete";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.alt="delete icon with cross sign";
   deleteButton.appendChild(deleteButtonImg);
 
 
